3d63bf880f265ffceb09aa596e197f5163b954bd
Maxim Lanin <max@lanin.me>
2016-10-19 12:45:43 +0300

    Update views texts

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -40,7 +40,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Page not found.</div>
+                <div class="title">{{ $exception->getMessage() }}</div>
             </div>
         </div>
     </body>
